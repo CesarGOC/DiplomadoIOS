76c54122ac24967e20c5ca05095e63bf20dd0d92
Cesar <cesar.yt2015@gmail.com>
2023-11-17 20:47:01 -0600

    Termino de app

--- ModuloIV/Pokedex2/Pokedex2/SceneDelegate.swift
+++ ModuloIV/Pokedex2/Pokedex2/SceneDelegate.swift
@@ -18,7 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: scene)
+        let pokemonListViewController = PokemonListTableViewController(style: .insetGrouped)
         window.rootViewController = PokemonListTableViewController()
+        let navigationController = UINavigationController(rootViewController: pokemonListViewController)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
     }

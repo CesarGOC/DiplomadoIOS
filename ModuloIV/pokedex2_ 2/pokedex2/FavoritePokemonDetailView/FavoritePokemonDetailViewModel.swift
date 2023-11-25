53cfbcae4ed9869766530d5c42e344fed4255ea9
Cesar <cesar.yt2015@gmail.com>
2023-11-25 14:13:13 -0600

    Add pokedex 2

---
+++ ModuloIV/pokedex2_ 2/pokedex2/FavoritePokemonDetailView/FavoritePokemonDetailViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritePokemonDetailViewModel.swift
+//  pokedex2
+//
+//  Created by Diplomado on 25/11/23.
+//
+
+import Foundation
+
+class FavoritePokemonDetailViewModel {
+    private let pokemon: Pokemon
+    
+    init(pokemon: Pokemon) {
+        self.pokemon = pokemon
+    }
+}
---
+++ ModuloIV/pokedex2_ 2/pokedex2/FavoritePokemonListView/FavoritePokemonListViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  FavoritePokemonListViewModel.swift
+//  pokedex2
+//
+//  Created by Diplomado on 25/11/23.
+//
+
+import Foundation
+
+class FavoritePokemonListViewModel {
+    private var favoritePokemonList: Set<Pokemon> = []
+    
+    var numberOfRows: Int { favoritePokemonList.count }
+    var numberOfSections: Int = 1
+    let favoriteCellIdentifier: String = "favoritePokemonIdentifier"
+    
+    init() {
+        loadFavoritePokemon()
+    }
+    
+    func pokemon(at indexPath: IndexPath) -> Pokemon {
+        let sortedFavoritePokemon = Array(favoritePokemonList).sorted { $0.id > $1.id }
+        return sortedFavoritePokemon[indexPath.row]
+    }
+    
+    private func loadFavoritePokemon() {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return
+        }
+        let fileName = "favorite_pokemon_list.json"
+        let favoritePokemonURL = documentDirectory.appending(path: fileName)
+        
+        do {
+            let favoritePoemonData = try Data(contentsOf: favoritePokemonURL)
+            let favoritePokemonList = try JSONDecoder().decode(Set<Pokemon>.self, from: favoritePoemonData)
+            self.favoritePokemonList = favoritePokemonList
+            
+        } catch {
+            //assertionFailure("cannot read data from \(fileName)")
+        }
+    }
+}
